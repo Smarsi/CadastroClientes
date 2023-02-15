61ecdb38f7ad947cb75902825cdb0d160e221556
Richard Smarsi <ricsmarsi@gmail.com>
2023-02-15 20:35:27 -0300

    Iniciada configuração do projeto. Adicionado o DJango Rest Framework na lista de installed Apps. Adicionado também o app core.

--- system/settings.py
+++ system/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!9@rd(jc&t81h2574izgu(5npum@2adrx*1!sm2=-+^emb(iwc'
+SECRET_KEY = 'django-insecure-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #third part installed apps
+    'rest_framework',
+
+    #my own apps
+    'core',
 ]
 
 MIDDLEWARE = [
