94c2163da26a0cd4908e2bd14070ce6cb7b00d4f
Richard Smarsi <ricsmarsi@gmail.com>
2023-02-15 21:43:30 -0300

    Configurando banco de dados com Models.py no app core

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Base(models.Model):
+    criados = models.DateField('Criação', auto_now_add = True)
+    modificado = models.DateField('Modificado', auto_now = True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Cliente(Base):
+    nome = models.CharField('Nome', max_length=200, blank=False, null=False)
+    cpf = models.IntegerField('CPF', max_length=11, blank=False, null=False)
+    nascimento = models.DateField('Data de Nascimento', auto_now=False, auto_now_add=False)
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_plural_name = 'Clientes'
+
+    def __str__(self):
+        return str(self.pk, self.nome, self.cpf, self.nascimento)
