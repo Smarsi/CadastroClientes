bdec6613aa482886f1bf925724313ac2fd3f3868
Richard Smarsi <ricsmarsi@gmail.com>
2023-02-15 21:19:28 -0300

    Mapeando primeiras urls que serão usadas no projeto

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import *
+
 urlpatterns = [
-    path('/', )
+    path('new-costumer/', ),
+    path('consult-all-costumers/', ),
+    path('consult-costumer/id=?', ),
 ]
