1548fd79b77294e5f9218dc44c52a5f2b02cefc4
Richard Smarsi <ricsmarsi@gmail.com>
2023-02-20 12:02:16 -0300

    Removido o model de endereco e o código de consumo na api do viacep. Por conta do tempo necessário para entregar o projeto não será possível finalizar a configuração e integração com as views de cadastro de clientes. Além disso, também achei que a inclusão de mais um módulo de verificação e validação poderia dificultar e atrapalhar a leitura e entendimento da validação de CPF que é o principal requisito do teste técnico como um todo. Entretanto, o cadastro de endereço usando o viacep pode se tornar um 'upgrade' no futuro.

--- core/models.py
+++ core/models.py
@@ -21,23 +21,3 @@ class Cliente(Base):
 
     def __str__(self):
         return str(self.nome + ' - CPF:'+ str(self.cpf))
-    
-class Endereco(Base):
-    cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
-    logradouro = models.CharField('Logradouro', max_length=200, null=False, blank=False)
-    numero = models.CharField('Numero', max_length=50, null=False, blank=False)
-    complemento = models.CharField('Complemento', max_length=50)
-    bairro = models.CharField('Bairro', max_length=100, null=False, blank=False)
-    cidade = models.CharField('Cidade', max_length=100, null=False, blank=False)
-    estado = models.CharField('Estado', max_length=100, null=False, blank=False)
-    cep = models.CharField('Cep', max_length=8, null=False, blank=False)
-
-    class Meta:
-        verbose_name = 'Endereço'
-        verbose_name_plural = 'Endereços'
-
-    def __str__(self):
-        if self.complemento:
-            return str(self.logradouro + ', nº'+self.numero+', '+self.complemento+', '+self.bairro+', '+self.cidade+', '+self.estado)
-        else:
-            return str(self.logradouro + ', nº'+self.numero+', '+self.bairro+', '+self.cidade+', '+self.estado)
--- core/serializers.py
+++ core/serializers.py
@@ -8,8 +8,3 @@ class ClienteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cliente
         fields = ['nome', 'cpf', 'nascimento']
-
-class EnderecoSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Endereco
-        fields = ['cliente', 'logradouro', 'numero', 'complemento', 'bairro', 'cidade', 'estado', 'cep']
